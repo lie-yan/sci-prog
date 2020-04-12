5d9d99c24a7463168b2548cf31b91b79a83cfa56
yanl <yanl@getui.com>
2020-04-12 11:39:30 +0800

    implement powi

--- num-rep/num_rep.cpp
+++ num-rep/num_rep.cpp
@@ -3,6 +3,34 @@
 
 
 /**
+ *  Given a base b, an exponent e, return b^e.
+ */
+int powi(int b, int e) {
+  // e0 := e
+  
+  int acc = 1;
+  // int d = 1;
+  int p = b;
+
+  // Invariant:
+  //    p = b^d
+  //    b^e0 = acc * p^e
+  // Note:
+  //    If e = 2m+1, then acc * p^e = acc * p^(2m+1) = (acc * p) * (p^2)^m.
+  //    If e = 2m, then acc * p^e = acc * p^(2m) = acc * (p^2)^m.
+  while (e > 0) {
+    if (e % 2) acc *= p;
+    // d += 1;
+    p *= p;
+    e /= 2;
+  }
+  
+  // acc = b^e0
+  return acc;
+}
+
+
+/**
  *  Given an integer x, a window size ell, return the decomposition of 
  *  x under windowing scheme. 
  */
@@ -13,5 +41,20 @@ std::vector<int> decompose_integer_windowing(int x, int ell) {
 
 
 int main() {
+  std::cout << powi(3, 0) << std::endl;
+  std::cout << powi(3, 1) << std::endl;
+  std::cout << powi(3, 2) << std::endl;
+  std::cout << powi(3, 3) << std::endl;
+  std::cout << powi(3, 4) << std::endl;
+  std::cout << powi(3, 5) << std::endl;
+
+  std::cout << powi(2, 0) << std::endl;
+  std::cout << powi(2, 1) << std::endl;
+  std::cout << powi(2, 2) << std::endl;
+  std::cout << powi(2, 3) << std::endl;
+  std::cout << powi(2, 4) << std::endl;
+  std::cout << powi(2, 5) << std::endl;
+
+  
   std::cout << "hello\n";
 }
