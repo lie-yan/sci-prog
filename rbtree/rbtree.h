b9214edaa2b2df43037d6a280ccc34242b9e3919
Lie Yan <robyn.lie.yan@outlook.com>
2020-04-14 07:50:21 +0800

    modify rbtree.h: add comments

--- rbtree/rbtree.h
+++ rbtree/rbtree.h
@@ -19,10 +19,6 @@ public:
     RED,
   };
 
-  std::optional<T> find(K key) const;
-  void insert(K key, V value);
-  void erase(K key);
-
 protected:
   struct Node {
     uint8_t color;
@@ -32,6 +28,32 @@ protected:
 
     T payload;
   };
+
+public:
+  std::optional<T> find(K key) const;
+  void insert(K key, V value);
+  void erase(K key);
+
+protected:
+  /**
+   *  Given a node in the search tree, return its predecessor node. 
+   *  
+   *  The rule is as follows.
+   *    1) If the node is null, return null.
+   *    2) Or, return its predeceessor if any.
+   *    3) Otherwise return null.
+   */
+  Node* predecessor(Node* t) const;
+  
+  /**
+   *  Given a node in the search tree, return its successor node. 
+   *  
+   *  The rule is as follows.
+   *    1) If the node is null, return null.
+   *    2) Or, return its successor if any.
+   *    3) Otherwise return null.
+   */
+  Node* successor(Node* t) const;
 
 private:
   Node* root;
