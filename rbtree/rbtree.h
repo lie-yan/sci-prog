f7af81c029edb9df1851f32ffb3ce320d68f7bd9
Lie Yan <robyn.lie.yan@outlook.com>
2020-04-13 22:01:03 +0800

    modify rbtree.h

--- rbtree/rbtree.h
+++ rbtree/rbtree.h
@@ -4,3 +4,22 @@
 
 #pragma once
 
+#include <cstdint>
+
+struct Node {
+  using T = int;
+
+
+  enum {
+    BLACK,
+    RED,
+  };
+
+  uint8_t color;
+  Node *p;
+  Node *left;
+  Node *right;
+
+  T payload;
+};
+
